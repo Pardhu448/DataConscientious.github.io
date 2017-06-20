129b6700957f6168546a0c4671f1849df480dc22
pardhu448 <pardhuuvce@gmail.com>
2017-06-21 01:08:33 +0530

    First major draft - Just place holder need to add content later

---
+++ _posts/2017-01-20-projectnew.md
@@ -0,0 +1,12 @@
+---
+layout: post
+title: "Sample Project - 01 "
+author: "partha"
+categories: projects
+tags: [open competition, kaggle]
+image:
+      feature :	sampleimg1.png
+---
+Every minute, the world loses an area of forest the size of 48 football fields. And deforestation in the Amazon Basin accounts for the largest share, contributing to reduced biodiversity, habitat loss, climate change, and other devastating effects. But better data about the location of deforestation and human encroachment on forests can help governments and local stakeholders respond more quickly and effectively.
+
+
---
+++ images/sample.py
@@ -0,0 +1,31 @@
+											from __future__ import division
+											import time
+
+											import numpy as np
+
+											from sklearn.svm import SVR
+											from sklearn.model_selection import GridSearchCV
+											from sklearn.model_selection import learning_curve
+											from sklearn.kernel_ridge import KernelRidge
+											import matplotlib.pyplot as plt
+
+											train_size = 100
+											svr = GridSearchCV(SVR(kernel='rbf', gamma=0.1), cv=5,
+											                   param_grid={"C": [1e0, 1e1, 1e2, 1e3],
+											                               "gamma": np.logspace(-2, 2, 5)})
+
+											kr = GridSearchCV(KernelRidge(kernel='rbf', gamma=0.1), cv=5,
+											                  param_grid={"alpha": [1e0, 0.1, 1e-2, 1e-3],
+											                              "gamma": np.logspace(-2, 2, 5)})
+
+											t0 = time.time()
+											svr.fit(X[:train_size], y[:train_size])
+											svr_fit = time.time() - t0
+											print("SVR complexity and bandwidth selected and model fitted in %.3f s"
+											      % svr_fit)
+
+											t0 = time.time()
+											kr.fit(X[:train_size], y[:train_size])
+											kr_fit = time.time() - t0
+											print("KRR complexity and bandwidth selected and model fitted in %.3f s"
+											      % kr_fit)
